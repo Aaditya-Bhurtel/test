952b58e79f51ab045cd42fcdcc16832a0be40577
Aaditya-Bhurtel <126585848+Aaditya-Bhurtel@users.noreply.github.com>
2023-02-28 20:12:46 +0545

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,252 @@
+let MCQS = [{
+    question: "If furniture of wood, shoes of leather, sugar of sugarcane, what will be of thread?",
+    choice1: "Cotton",
+    choice2: "Fabric",
+    choice3: "silk",
+    choice4: "Clothes",
+    answer: 0
+},
+            {
+            question: "Which is the softest stone?",
+    choice1: "Rock",
+    choice2: "Sand",
+    choice3: "Gravel",
+    choice4: "Coral",
+    answer: 1
+},
+            {
+            question: "She is my maternal grandfather's daughter's daughter-in-law then what is she to me",
+    choice1: "Mom",
+    choice2: "Sister",
+    choice3: "Wife",
+    choice4: "Dughter",
+    answer: 2
+},
+            {
+            question: "5 is multiplied by 5 and divided by 5. Then 5 is substracted from that and again multiplied by 5 what number is it?",
+    choice1: "0",
+    choice2: "1",
+    choice3: "5",
+    choice4: "25",
+    answer: 0
+},
+            {
+            question: "How many black rooms are there in chess board?",
+    choice1: "8",
+    choice2: "16",
+    choice3: "32",
+    choice4: "64",
+    answer: 2
+},
+            {
+            question: "If the baby duck is called duckling then what is a baby rhino called?",
+    choice1: "Rhily",
+    choice2: "Kid",
+    choice3: "Calf",
+    choice4: "Cub",
+    answer: 2
+},
+            {
+               question: "Which sea animal never gets sick?",
+    choice1: "Sea Turtle",
+    choice2: "Shark",
+    choice3: "Fish",
+    choice4: "Whale",
+    answer: 1
+},
+{
+               question: "What is the Full form of 'IQ' ",
+    choice1: "Intellectual Question",
+    choice2: "Intelligent Quotient",
+    choice3: "Intellectual Quotient",
+    choice4: "Intelligent Question",
+    answer: 1
+},
+            {
+               question: "If yesterday's day before yesterday was sunday which day will be tomorrow's day after tomorrow?",
+    choice1: "Sunday",
+    choice2: "Thursday",
+    choice3: "Friday",
+    choice4: "Saturday",
+    answer: 3
+    
+},
+            {
+               question: "There are trees in a line in one forest. While counting trees a tree which is in the 40th position from left, is in the 15th position from the right. then how many trees are there in that line?",
+    choice1: "53",
+    choice2: "54",
+    choice3: "55",
+    choice4: "56",
+    answer: 1
+
+},
+/*10 questions*/ 
+{
+       question: " Book is to reading as fork is to:",
+       choice1: "Cooking",
+       choice2: "Eatting",
+       choice3: "Stirring",
+       choice4: "Drawing",
+       answer: 1
+},
+{
+               question: "Which number is missing : 1, 4, 9, 16, 25, ?, 49, 64, 81",
+    choice1: "30",
+    choice2: "32",
+    choice3: "34",
+    choice4: "36",
+    answer: 3
+},
+
+{    
+     question: "There are 5 machines that make 5 parts in 5 minutes. How long does it take to make 100 parts on 100 machines?",
+     choice1: "5",
+     choice2: "25",
+     choice3: "75",
+     choice4: "100",
+     answer: 0
+},
+
+
+{    
+     question: "8:32 as 5: ?",
+     choice1: "10",
+     choice2: "15",
+     choice3: "20",
+     choice4: "25",
+     answer: 2
+},
+{    
+     question: "How many letters are there in the alphabet",
+     choice1: "24",
+     choice2: "25",
+     choice3: "26",
+     choice4: "27",
+     answer: 2
+},
+{    
+     question: "What is the molecular name of water",
+     choice1: "dihydrogen oxide",
+     choice2: "hydrogen oxide",
+     choice3: "hydrogen trioxide",
+     choice4: "hydrogen dioxide",
+     answer: 0
+},
+{    
+     question: "What is not the opposite of 'EASY'",
+     choice1: "HARD",
+     choice2: "DIFFICULT",
+     choice3: "COMPLICATED",
+     choice4: "EFFORTLESS",
+     answer: 3
+},
+{    
+     question: "The more I drink water the faster I will die. What am I? ",
+     choice1: "Flour",
+     choice2: "Sugar",
+     choice3: "Salt",
+     choice4: "Thirst",
+     answer: 3
+},
+{    
+     question: "In which ocean is the Deepest oceanic trench on Earth 'Mariana Trench' located ? " ,
+     choice1: "Pacific",
+     choice2: "Indian",
+     choice3: "Atlantic",
+     choice4: "Southern",
+     answer: 0
+},
+{    
+     question: "I’m tall when I’m young, and I’m short when I’m old. What am I?",
+     choice1: "Tree",
+     choice2: "Nail",
+     choice3: "Hair",
+     choice4: "Candel",
+     answer: 3
+},
+/*20 questions*/
+{    
+     question: "Approx how many years ago did the big bang occur ?",
+     choice1: "13.5 billion",
+     choice2: "13.6 billion",
+     choice3: "13.7 billion",
+     choice4: "13.8 billion",
+     answer: 3
+},
+{    
+     question: "How many countries are there in the world ?",
+     choice1: "190",
+     choice2: "196",
+     choice3: "199",
+     choice4: "200",
+     answer: 1
+},
+{    
+     question: "How many letters are there in the 'alphabet'",
+     choice1: "8",
+     choice2: "16",
+     choice3: "26",
+     choice4: "32",
+     answer: 0
+},
+{    
+     question: "What is the scientific name of 'Human'",
+     choice1: "Hame Sappin",
+     choice2: "Homo Dapens",
+     choice3: "Homo Sapiens",
+     choice4: "Domos Sapiens",
+     answer: 2
+},
+{    
+     question: "What is half of quater of 8000",
+     choice1: "1000",
+     choice2: "2000",
+     choice3: "4000",
+     choice4: "400",
+     answer: 0
+},
+{    
+     question: "PEACH is to HCAEP as 35791 is to ",
+     choice1: "54729",
+     choice2: "14738",
+     choice3: "14653",
+     choice4: "19753",
+     answer: 3
+},
+{    
+     question: "Pick the odd one: EARS, EYES, NOSE, LIPS",
+     choice1: "EARS",
+     choice2: "EYES",
+     choice3: "NOSE",
+     choice4: "LIPS",
+     answer: 2
+},
+{    
+     question: "Which is the most followed religion in the world?",
+     choice1: "Christian",
+     choice2: "Islam",
+     choice3: "Hindu",
+     choice4: "Buddhism",
+     answer:0 
+},
+{    
+     question: "Ram has 4 daughters and each daughter has a brother. In total how many children does he have ? ",
+     choice1: "9",
+     choice2: "7",
+     choice3: "5",
+     choice4: "4",
+     answer: 2
+},
+{    
+     question: "8 / 4 + 8(4-2) = ? ",
+     choice1: "20",
+     choice2: "14",
+     choice3: "16",
+     choice4: "18",
+     answer: 3
+},
+
+
+
+
+];
